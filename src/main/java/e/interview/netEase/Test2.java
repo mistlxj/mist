a2d45636b40fb3f7f043dcfad7374febb47c2614
xiaojian.li <xiaojian.li@ucarinc.com>
2017-11-13 18:54:42 +0800

    add netEase interview tests

--- src/main/java/e/interview/netEase/Test2.java
+++ src/main/java/e/interview/netEase/Test2.java
@@ -1,9 +1,11 @@
 package e.interview.netEase;
 public class Test2 {
+
     public static void main(String[] args) {
         Test2 t2 = new Test2();
         t2.test();
     }
+
     public void test() {
         int i = 99;
         Temp tmp = new Temp();
@@ -11,12 +13,14 @@ public class Test2 {
         testTest(tmp, i);
         System.out.println(tmp.i);
     }
+
     private void testTest(Temp tmp, int i) {
         i = 0;
         Temp t = new Temp();
         tmp = t;
         System.out.println(tmp.i + " " + i);
     }
+
     class Temp {
         int i = 10;
     }
